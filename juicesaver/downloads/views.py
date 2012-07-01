107dc53a1516957080866f052d6f45f5f0bd1516
Ross Masters <ross@rossmasters.com>
2012-07-01 01:03:21 +0100

    Created templates/ (first on lookup path), tested base inheritance in downloads

--- juicesaver/downloads/views.py
+++ juicesaver/downloads/views.py
@@ -1,6 +1,6 @@
 from downloads import downloads_bp
+from flask import render_template
 
 @downloads_bp.route('/')
 def list_downloads():
-    return "list_downloads"
-
+    return render_template('list_downloads.html')
